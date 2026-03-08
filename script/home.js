bd655317f55c29244d9c3d14ebedbbf19b4413d2
IamPial <pialname394@gmail.com>
2026-03-08 15:31:35 +0600

    show All the issues with functionality

--- script/home.js
+++ script/home.js
@@ -3,6 +3,7 @@ const allBtn = document.getElementById("all");
 const openBtn = document.getElementById("open");
 const closedBtn = document.getElementById("closed");
 const issuesTitle = document.getElementById("issues-title");
+const allCards = document.getElementById("all-cards");
 
 async function showActive(id) {
   const res = await fetch(
@@ -40,3 +41,74 @@ async function showActive(id) {
     issuesTitle.innerText = closedIssues.length + " " + "Issues";
   }
 }
+
+// load single  issues
+const loadIssues = async () => {
+  const res = await fetch(
+    "https://phi-lab-server.vercel.app/api/v1/lab/issues",
+  );
+  const data = await res.json();
+  ShowAllIssues(data.data);
+};
+
+function ShowAllIssues(issues) {
+  allCards.innerHTML = "";
+  issues.forEach((issue) => {
+    const div = document.createElement("div");
+    div.className =
+      "card bg-base-100 shadow-md rounded-xl border border-gray-200";
+    div.innerHTML = `
+     <div class="card-body p-0 border-t-3 rounded-xl  ${issue.status == "open" ? "border-green-500" : "border-purple-500"} ">
+              <div class="p-4 space-y-3 h-full">
+                <div class="flex justify-between items-center">
+                  <img src="${issue.status == "closed" ? "./assets/Closed-Status.png" : "./assets/Open-Status.png"}" />
+
+                  ${
+                    issue.priority == "high"
+                      ? `<span
+                    class="badge badge-error bg-red-100 text-error border-0 rounded-full uppercase px-6"
+                    >${issue.priority}</span>`
+                      : `${
+                          issue.priority == "medium"
+                            ? `<span
+      class="badge badge-error bg-yellow-100 text-yellow-500 border-0 rounded-full uppercase px-6 py-3.5">${issue.priority}
+    </span>`
+                            : `<span class="badge badge-error bg-gray-200 text-gray-400 border-0 rounded-full uppercase px-6 py-3.5">${issue.priority}</span>`
+                        }`
+                  }
+                </div>
+                <div class="h-24">
+                  <h2 class="card-title font-semibold text-[16px] ">
+                    ${issue.title}
+                  </h2>
+                  <p class="line-clamp-2 text-sm text-gray-500">
+                    ${issue.description}
+                  </p>
+                </div>
+                <div class="card-actions justify-start ">
+                  <div
+                    class="badge badge-outline bg-red-100 text-error rounded-full uppercase px-4 py-3.5"
+                  >
+                    <i class="fa-solid fa-bug"></i>
+                    Bug
+                  </div>
+                  <div
+                    class="badge badge-warning bg-yellow-100 text-yellow-600 rounded-full uppercase px-4 py-3.5"
+                  >
+                    <i class="fa-regular fa-life-ring"></i>
+                    help wanted
+                  </div>
+                </div>
+              </div>
+              <div class="bg-gray-300 w-full h-0.5"></div>
+              <div class="p-4 space-y-2">
+                <p class="text-sm text-gray-500">#1 by ${issue.author}</p>
+                <p class="text-sm text-gray-500">${new Date(issue.createdAt).toLocaleDateString()}</p>
+              </div>
+            </div>
+
+    `;
+    allCards.appendChild(div);
+  });
+}
+loadIssues();
